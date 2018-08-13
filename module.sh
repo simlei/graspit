61eb8c3efdc1e15ef61ef707e29c4186495d66b5
Simon Leischnig <simon-jena@gmx.net>
2018-08-13 19:17:21 +0200

    touchstone files

---
+++ module.sh
@@ -0,0 +1,6 @@
+export TS_MODULES="$mod_graspit_root:$TS_MODULES"
+
+export GRASPIT="$HOME/.graspit"
+
+mod_graspit_localprefix="$mod_graspit_root/.local"
+mod_graspit_builddir="$mod_graspit_root/build"
