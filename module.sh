5c6bec45ebc353e1506db3971ff28345423fbe5e
Simon Leischnig <simon-jena@gmx.net>
2018-08-17 20:50:12 +0200

    exported vital env variabled

--- module.sh
+++ module.sh
@@ -2,5 +2,7 @@ export TS_MODULES="$mod_graspit_root:$TS_MODULES"
 
 export GRASPIT="$HOME/.graspit"
 
-mod_graspit_localprefix="$mod_graspit_root/.local"
-mod_graspit_builddir="$mod_graspit_root/build"
+export mod_graspit_localprefix="$mod_graspit_root/.local"
+export mod_graspit_builddir="$mod_graspit_root/build"
+
+export mod_graspit_root
